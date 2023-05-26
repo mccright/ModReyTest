fcfb82bf6bdabd991a5b53d9784042728642b759
mccright <6339517+mccright@users.noreply.github.com>
2023-05-26 15:09:42 -0500

    Update modreytest.py
    Wrapped a print line to address a linter 'line-too-long' warning

--- modreytest.py
+++ modreytest.py
@@ -2,6 +2,10 @@ import random
 import time
 import os
 
+# See the readme.md at:
+# https://github.com/mccright/ModReyTest/blob/main/README.md
+
+
 def get_words(file_name: str, number_of_words: int) -> list:
     # Read the contents of the file then load them into a list
     with open(file_name, 'r', encoding="utf-8") as file:
@@ -11,6 +15,7 @@ def get_words(file_name: str, number_of_words: int) -> list:
     selected_words = random.sample(words, number_of_words)
     return selected_words
 
+
 def print_words(present_these_words: list, seconds_on_screen: int):
     # Print one of the selected words every 'seconds_on_screen' seconds
     for word in present_these_words:
@@ -18,6 +23,7 @@ def print_words(present_these_words: list, seconds_on_screen: int):
         time.sleep(seconds_on_screen)
         # Clear the screen before the next word
         os.system('cls' if os.name == 'nt' else 'clear')
+
 
 # Word list from Google:
 # https://github.com/first20hours/google-10000-english/blob/master/20k.txt
@@ -40,6 +46,7 @@ user_input = input("Enter as many of the words you just saw as you can, separate
 
 # Compare the user input with the 'present_these_words' words and print matches
 matches = [word for word in user_input.split() if word in presentthesewords]
-print(f"\n{str(len(matches))} of your words matched one of the {str(len(presentthesewords))} words we presented:")
+print(f"\n{str(len(matches))} of your words matched one of \
+the {str(len(presentthesewords))} words we presented:")
 print(f"\nYou matched: {matches}")
 print(f"\nWe had presented: {presentthesewords}")
