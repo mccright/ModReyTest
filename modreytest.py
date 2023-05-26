cdaba4104d6431b97fc019cc7201eed25d0da0e0
mccright <6339517+mccright@users.noreply.github.com>
2023-05-26 14:43:35 -0500

    Update modreytest.py
    Changes to satisfy linter

--- modreytest.py
+++ modreytest.py
@@ -2,13 +2,13 @@ import random
 import time
 import os
 
-def get_words(filename: str, numberofwords: int) -> list:
+def get_words(file_name: str, number_of_words: int) -> list:
     # Read the contents of the file then load them into a list
-    with open(filename, 'r') as file:
+    with open(file_name, 'r', encoding="utf-8") as file:
         words = [line.strip() for line in file]
 
     # 'Randomly' select twenty words from the list
-    selected_words = random.sample(words, numberofwords)
+    selected_words = random.sample(words, number_of_words)
     return selected_words
 
 def print_words(present_these_words: list, seconds_on_screen: int):
@@ -19,27 +19,27 @@ def print_words(present_these_words: list, seconds_on_screen: int):
         # Clear the screen before the next word
         os.system('cls' if os.name == 'nt' else 'clear')
 
-# Word list from Google: 
-# https://github.com/first20hours/google-10000-english/blob/master/20k.txt 
-# ToDo: I need a list better taylored to a ModRey test -- where using 
-# semantically and phonemically unrelated words improves the 
-# quality of test results 
-filename = '20k.txt'
-numberofwords = 20
-seconds_on_screen = 3
+# Word list from Google:
+# https://github.com/first20hours/google-10000-english/blob/master/20k.txt
+# ToDo: I need a list better taylored to a ModRey test -- where using
+# semantically and phonemically unrelated words improves the
+# quality of test results
+FILENAME = '20k.txt'
+NUMBEROFWORDS = 20
+SECONDSONSCREEN = 3
 
 # Clear the screen to start
 os.system('cls' if os.name == 'nt' else 'clear')
 
-present_these_words = get_words(filename, numberofwords)
+presentthesewords = get_words(FILENAME, NUMBEROFWORDS)
 
-print_words(present_these_words, seconds_on_screen)
+print_words(presentthesewords, SECONDSONSCREEN)
 
-# Ask the user to input the words they remember 
+# Ask the user to input the words they remember
 user_input = input("Enter as many of the words you just saw as you can, separated by spaces: ")
 
 # Compare the user input with the 'present_these_words' words and print matches
-matches = [word for word in user_input.split() if word in present_these_words]
-print(f"\n{str(len(matches))} of your words matched one of the {str(len(present_these_words))} words we presented:")
+matches = [word for word in user_input.split() if word in presentthesewords]
+print(f"\n{str(len(matches))} of your words matched one of the {str(len(presentthesewords))} words we presented:")
 print(f"\nYou matched: {matches}")
-print(f"\nWe had presented: {present_these_words}")
+print(f"\nWe had presented: {presentthesewords}")
